96aa83540daff99d61313a8bee5172b64b503c6a
mn-48 <nazmul.cse48@gmail.com>
2024-07-31 13:16:34 +0600

    pagination user

--- soa-python/admin/users/serializers.py
+++ soa-python/admin/users/serializers.py
@@ -3,6 +3,8 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
+from datetime import datetime, timedelta
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,9 +24,24 @@ class UserSerializer(serializers.ModelSerializer):
         # print("Send OTP/Link ....")
         return user
 
+# class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+#         token['email'] = user.email
+#         return token
+
+
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['email'] = user.email
-        return token
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Add issued and expiration times
+        data['originate_at'] = datetime.now().isoformat()  # Current time in ISO format
+
+        expire = datetime.now() + api_settings.ACCESS_TOKEN_LIFETIME
+        data['expire_at'] = expire.isoformat()  # Expiration time in ISO format
+
+        return data
